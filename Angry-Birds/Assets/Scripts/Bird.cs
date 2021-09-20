c196cc20e22568d2eaa37b7be0d8ad6e1d9bb4f3
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-09-20 20:25:01 +0700

    bird scripting

---
+++ Angry-Birds/Assets/Scripts/Bird.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public enum BirdState { Idle, Thrown}
+    public GameObject parent;
+    public Rigidbody2D rigidBody;
+    public CircleCollider2D birdCollider;
+
+    private BirdState _state;
+    private float _minVelocity = 0.05f;
+    private bool _flagDestroy = false;
+
+    private void Start()
+    {
+        rigidBody.bodyType = RigidbodyType2D.Kinematic;
+        birdCollider.enabled = false;
+        _state = BirdState.Idle;
+    }
+
+    private void FixedUpdate()
+    {
+        if(_state == BirdState.Idle && rigidBody.velocity.sqrMagnitude >= _minVelocity)
+        {
+            _state = BirdState.Thrown;
+        }
+        if(_state == BirdState.Thrown && rigidBody.velocity.sqrMagnitude < _minVelocity && !_flagDestroy)
+        {
+            _flagDestroy = true;
+            StartCoroutine(DestroyAfter(2));
+        }
+    }
+
+    private IEnumerator DestroyAfter(float second)
+    {
+        yield return new WaitForSeconds(second);
+        Destroy(gameObject);
+    }
+
+    public void MoveTo(Vector2 target, GameObject parent)
+    {
+        gameObject.transform.SetParent(parent.transform);
+        gameObject.transform.position = target;
+    }
+
+    public void Shoot(Vector2 velocity, float distance, float speed)
+    {
+        birdCollider.enabled = true;
+        rigidBody.bodyType = RigidbodyType2D.Dynamic;
+        rigidBody.velocity = velocity * speed * distance;
+    }
+}
