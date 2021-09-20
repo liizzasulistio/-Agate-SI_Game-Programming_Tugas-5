80b253c0f973779d20511df794e381e775f449a2
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-09-20 20:12:09 +0700

    add slingshooter scripting

---
+++ Angry-Birds/Assets/Scripts/SlingShooter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingShooter : MonoBehaviour
+{
+    public CircleCollider2D collider;
+    private Vector2 _startPos;
+
+    [SerializeField] private float _radius = 0.75f;
+    [SerializeField] private float _throwSpeed = 30f;
+
+    private void Start()
+    {
+        _startPos = transform.position;
+    }
+
+    private void OnMouseUp()
+    {
+        collider.enabled = false;
+        Vector2 velocity = _startPos - (Vector2)transform.position;
+        float distance = Vector2.Distance(_startPos, transform.position);
+
+        gameObject.transform.position = _startPos;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector2 p = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 dir = p - _startPos;
+        if (dir.sqrMagnitude > _radius) dir = dir.normalized * _radius;
+        transform.position = _startPos + dir;
+    }
+}
