0e0cee51edeb483f6d00084a4a8246311222221f
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-09-23 17:13:39 +0700

    scripting enemy

--- Angry-Birds/Assets/Scripts/GameController.cs
+++ Angry-Birds/Assets/Scripts/GameController.cs
@@ -6,7 +6,9 @@ public class GameController : MonoBehaviour
 {
     public SlingShooter slingShooter;
     public List<Bird> birds;
+    public List<Enemy> enemies;
 
+    private bool _isGameEnded = false;
     
 
     private void Start()
@@ -15,14 +17,39 @@ public class GameController : MonoBehaviour
         {
             birds[i].OnBirdDestroyed += ChangeBird;
         }
+
+        for(int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].OnEnemyDestroyed += CheckGameEnd;
+        }
         slingShooter.InitiateBird(birds[0]);
     }
 
     public void ChangeBird()
     {
+        if(_isGameEnded)
+        {
+            return;
+        }
         birds.RemoveAt(0);
 
         if (birds.Count > 0) slingShooter.InitiateBird(birds[0]);
+    }
+
+    public void CheckGameEnd(GameObject destroyedEnemy)
+    {
+        for(int i = 0; i < enemies.Count; i++)
+        {
+            if(enemies[i].gameObject == destroyedEnemy)
+            {
+                enemies.RemoveAt(i);
+                break;
+            }
+        }
+        if(enemies.Count == 0)
+        {
+            _isGameEnded = true;
+        }
     }
 
 }
